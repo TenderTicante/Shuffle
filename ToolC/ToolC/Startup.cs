0d3a78dee2570bf420fa63204a836398796cb77d
Tender Ticante <tenderticante@gmail.com>
2018-02-03 21:21:27 -0800

    Halp

---
+++ ToolC/ToolC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ToolC.Startup))]
+namespace ToolC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
